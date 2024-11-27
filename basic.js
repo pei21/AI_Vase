2e7dcfa5f345e9d278336025c62d0dfd4d57eda3
pei21 <pj5581766@gmail.com>
2024-11-27 09:03:53 -0500

    update
    menu-icon-update

--- basic.js
+++ basic.js
@@ -1,7 +1,14 @@
-// Toggle Navigation Menu
+// Get the elements
 const menuToggle = document.getElementById("menu-toggle");
 const navLinks = document.getElementById("nav-links");
+const closeMenu = document.getElementById("close-menu");
 
+// Open and close the menu on menuToggle click
 menuToggle.addEventListener("click", () => {
   navLinks.classList.toggle("active");
+});
+
+// Close the menu when the close icon is clicked
+closeMenu.addEventListener("click", () => {
+  navLinks.classList.remove("active");
 });
